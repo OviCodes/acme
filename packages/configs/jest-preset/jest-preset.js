a620a7c34a2e27c1cd59279e187d923ea606ddcb
Ovi Ispas <ovi@ovi.codes>
2022-06-07 08:56:08 +0300

    feat(root): add run scripts and turborepo

--- packages/configs/jest-preset/jest-preset.js
+++ packages/configs/jest-preset/jest-preset.js
@@ -11,13 +11,13 @@ module.exports = {
     '!src/**/*.stories.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
   ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
   coverageProvider: 'v8',
 }
